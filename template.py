938789f271c376c72e187a5697838dfc8dba3927
Martica <58595345+Martica199005@users.noreply.github.com>
2021-11-02 19:20:29 +0100

    Create template.py

---
+++ template.py
@@ -0,0 +1,9 @@
+class ClassName:
+    """Documentation for the class."""
+    def method_name(self, other_parameters):
+        """Documentation for the method."""
+        body_of_method
+        
+def function_name(parameters):
+    """Documentation for the function."""
+    body_of_function
